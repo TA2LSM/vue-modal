52ae458dd454695dede2781894fbc74972409bcd
TA2LSM <semihsenol@gmail.com>
2023-03-12 16:49:54 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
+<!-- This is main route component (App.vue) -->
+<!-- Template is a mandatory part -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1><br>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me!</button>
+  <Modal />
 </template>
 
+
+<!--  These below are optional -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+   
   components: {
-    HelloWorld
-  }
+    Modal
+  },
+
+  data() {
+    return {
+      title: "~ First Vue Project ~"
+    }
+  },
+
+  methods: {
+    handleClick() {
+      // using "refs"
+      // console.log(this.$refs.named);
+      // console.log(this.$refs.name.value);
+      // this.$refs.name.classList.add('active');
+      // this.$refs.name.focus();
+    }
+  },
 }
 </script>
 
+<!-- These styles below will be injected in head section in html file -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,5 +46,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,60 @@
+<!-- Do not give standard HTML keyword names to your components
+when you use lowercase letters as a compoennet name or start
+naming them with capital letters !!! -->
+
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <h1>Modal Name</h1>
+      <p>Modal component</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'Modal',
+//   props: {
+//     msg: String
+//   }
+// }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .modal {
+    width: 400px;
+    padding:20px;
+    margin: 200px auto;
+    background: white;
+    border-radius: 10px;
+  }
+
+  .backdrop {
+    top: 0;
+    left: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+
+  /* h1 style owerwrite but all h1 tags will be applied unless you use "<style scoped>" above
+    In that case styles in this file will be renamed like this: <h1 data-v-xxxxxxxx> ... 
+    But this approach should be used in some specific situations not commonly */
+  /* h1 {
+    color: #3603cf;
+    border: none;
+    padding: 0;
+  } */
+
+  .modal h1 {
+    color: #3603cf;
+    border: none;
+    padding: 0;
+  }
+
+  .modal p {
+    font-style: normal;
+  }
+</style>
